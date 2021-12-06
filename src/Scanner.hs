f319898923b1323219680e96bd34c45d78698c8f
Reid Chen <mrr1vfe@gmail.com>
2021-12-06 17:20:13 -0600

    add comments to Scanner

--- src/Scanner.hs
+++ src/Scanner.hs
@@ -12,17 +12,26 @@ import Control.Monad (void)
 import Control.Applicative (empty)
 import Data.String.Conversions
 
+-- Set the custom error component to Void
+-- Set the type of the input stream to Text
+-- Text is more efficient than String == [Char]
 type Parser = Parsec Void Text
 
+-- A parser that can parses whitespaces
+-- space1 skips one or more white space characters.
+-- L.space allows patterns for ignoring comments
 spaceConsumer :: Parser ()
 spaceConsumer = L.space space1 lineComment blockComment
   where
     lineComment = L.skipLineComment "///" <|> L.skipLineComment "###"
+    -- empty means no blockComment allowed
     blockComment = empty
-    
+
+-- Supply spaceConsumer to L.lexmeme to create a parser for tokens
 lexeme :: Parser a -> Parser a
 lexeme = L.lexeme spaceConsumer
 
+-- Supply spaceConsumer to L.symbol to create a helper parser for symbols
 symbol :: Text -> Parser Text
 symbol = L.symbol spaceConsumer
 
@@ -44,6 +53,7 @@ semicolon = void $ symbol ";"
 comma :: Parser ()
 comma = void $ symbol ","
 
+-- try to parse reservedWords, these words should not followed by any other characters (alphaNumChar)
 reservedWord :: Text -> Parser ()
 reservedWord w = (lexeme . try) (string w *> notFollowedBy alphaNumChar)
 
@@ -62,19 +72,22 @@ reservedWords =
   ,"while"
   ,"ret"]
 
+
 strLit :: Parser Text
 strLit = do
+  -- string is enclosed by double quotes
+  -- The predicate use to test tokens is not "
+  -- Default to empty string
   content <- doubleQuotes $ takeWhileP Nothing (/= '"')
-  pure $ T.pack (read ('"' : cs content ++ "\""))
-
-charLit :: Parser Int
-charLit =
-  singleQuotes $ (ord <$> satisfy (`notElem` ['\\', '\''])) <|> (single '\\' >> int)
+  -- T.pack convert a String into a Text
+  pure $ T.pack ('"' : cs content ++ "\"")
 
 identifier :: Parser Id
 identifier = (lexeme . try) (p >>= check)
   where
+    -- parse first
     p = fmap T.pack $ (:) <$> letterChar <*> many (alphaNumChar <|> single '_')
+    -- then check the validity
     check x = if x `elem` reservedWords
               then fail $ "keyword " <> show x <> " cannot be an identifier"
               else return $ Id x
@@ -84,5 +97,3 @@ int = lexeme L.decimal
 
 float :: Parser Double
 float = lexeme L.float
-
-
